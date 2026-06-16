62283b3242610a4ae20fdfe5e5261bb820eeebe9
leakarttunen4-gif <leakarttunen4@gmail.com>
2026-06-16 09:58:04 +0300

    Add files via upload

---
+++ db.php
@@ -0,0 +1,9 @@
+<?php
+$pdo = new PDO(
+    "mysql:host=localhost;dbname=pielisenpyora;charset=utf8mb4",
+    "pyora",
+    "NrQIZZntDgwW3luldXYL",
+    [
+        PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
+    ]
+);
